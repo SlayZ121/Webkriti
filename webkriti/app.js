ef9178044b95c6738259bc77fb2066eb816f852f
dork_boy <slayz9168@gmail.com>
2023-08-19 19:33:15 +0530

    initial commit

--- webkriti/app.js
+++ webkriti/app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const port = 3000;
-
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -10,41 +8,57 @@ mongoose.connect('mongodb+srv://dork_boy:the_weeknd<3@web.shju5d2.mongodb.net/ne
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
-    console.log('connected:');
-  })
-  .catch(error => {
+  console.log('Connected to MongoDB');
+}).catch(error => {
+  console.error('Error:', error);
+});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.use(express.static('hc'));
+
+app.get('/signup', (req, res) => {
+  res.sendFile(__dirname + '/hc/signup.html');
+});
+
+app.post('/signup', (req, res) => {
+  const { email, password } = req.body;
+
+  const newUser = new User({
+    email: email,
+    password: password
+  });
+
+  newUser.save().then(savedPerson => {
+    res.sendFile(__dirname + '/hc/home.html');
+  }).catch(error => {
     console.error('Error:', error);
   });
+});
 
-  const userSchema = new mongoose.Schema({
-    email: String,
-    password: String
-  });
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/hc/signin.html');
+});
 
-  const User = mongoose.model('User', userSchema);
-  
-  app.use(express.static('hc'));
+app.post('/login', async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
 
-  app.get('/signup',(req,res)=>{
-      res.sendFile(__dirname+"/hc/signin.html");
-  })
-  app.post('/signup',(req, res) => {
-    const { email, password } = req.body;
-  
-    const newUser = new User({
-      email: email,
-      password: password
-    });
-    newUser.save()
-  .then(savedPerson => {
-    res.sendFile(__dirname+"/hc/home.html")
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-    
-    });
+  const user = await User.findOne({ email, password });
 
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
-      });
+  if (user) {
+    res.sendFile(__dirname + '/hc/home.html');
+  } else {
+    res.redirect('/login?error=' + encodeURIComponent('Invalid email or password. Please try again.'));
+  }
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
